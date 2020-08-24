f026d13d27625cf81c5292f96f20b1f5741cdd40
Liam Donohue <liamd@naturallygrownkids.org>
2020-08-24 17:14:29 +0000

    dev

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+"use strict";
 const express = require('express');
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.sendfile('Public/index.html')
-});
+app.use(express.static("./Public", {index: `index.html`}));
 
 app.listen(3000, () => {
-  console.log('server started');
+  console.log('Zoom Server Booted');
+  console.log('This is a Public Server. All Content is protected under the GNU General Public License 3.0')
 });
